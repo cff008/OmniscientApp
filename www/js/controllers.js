1b850e426b7aa7ce065138af90f118a6ab496a55
MSpelman <michaelspelman@Michaels-MacBook-Pro.local>
2016-04-02 20:09:00 -0500

    Update
    Messages
    - Added date/time stamp

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,45 +11,45 @@ angular.module('app.controllers', ['ionic'])
       imgSrc: "img/Boc83lhjQNq0NXuhM6sl_nigel.jpg",
       lastMessage: "Hey, how do I set up Ionic?",
       messages: [
-        { user: 'Nigel', content: "Hey man what time are we meeting." },
-        { user: 'Conor', content: "6 pm in the CS Lab." },
-        { user: 'Nigel', content: "Okay i'll be there." },
-        { user: 'Nigel', content: "Hey, how do I set up Ionic?" }
+        { user: 'Nigel', content: "Hey man what time are we meeting.", timestamp: '4/4 @ 2:30pm' },
+        { user: 'Conor', content: "6 pm in the CS Lab.", timestamp: '4/4 @ 2:35pm' },
+        { user: 'Nigel', content: "Okay i'll be there.", timestamp: '4/4 @ 2:40pm' },
+        { user: 'Nigel', content: "Hey, how do I set up Ionic?", timestamp: '4/7 @ 7:30am' }
       ]
     },
     { name: 'CS570 Class Chat',
       imgSrc: "img/8607Y3KSIStiNw1U9Dom_tracy.jpg",
       lastMessage: "We have a quiz tomorrow.",
       messages: [
-        { user: 'Professor', content: "We have a quiz tomorrow." }
+        { user: 'Professor', content: "We have a quiz tomorrow.", timestamp: '4/5 @ 12:14pm' }
       ]
     },
     { name: 'CS506 Project 1',
       imgSrc: "img/AKScFVblSK5iHpV6pstq_Drawing.png",
       lastMessage: "Is everyone's GitHub accounts working?",
       messages: [
-        { user: 'Sven', content: "Is everyone's GitHub accounts working?" }
+        { user: 'Sven', content: "Is everyone's GitHub accounts working?", timestamp: '4/6 @ 2:30pm' }
       ]
     },
     { name: 'CS302 Class Chat',
       imgSrc: "img/1OqyVb4XQz6WYCPraFqR_Lab.jpg",
       lastMessage: "I believe it is due one week from today",
       messages: [
-        { user: 'Matt', content: "I believe it is due one week from today" }
+        { user: 'Matt', content: "I believe it is due one week from today", timestamp: '4/1 @ 3:46pm' }
       ]
     },
     { name: 'CS302 Group Chat',
       imgSrc: "img/tklIc2TaTMGodAGytjz7_Barry.jpg",
       lastMessage: "I'll meet you in the CS lobby at 5",
       messages: [
-        { user: 'Taylor', content: "I'll meet you in the CS lobby at 5" }
+        { user: 'Taylor', content: "I'll meet you in the CS lobby at 5", timestamp: '4/4 @ 2:30pm' }
       ]
     },
     { name: 'UW Programming Help',
       imgSrc: "img/5uhMge7TTyenmgf0lnzl_Tiki.jpg",
       lastMessage: "I think Node.JS will work better than Angular",
       messages: [
-        { user: 'Tiki', content: "I think Node.JS will work better than Angular" }
+        { user: 'Tiki', content: "I think Node.JS will work better than Angular", timestamp: '4/4 @ 2:30pm' }
       ]
     }
   ];
